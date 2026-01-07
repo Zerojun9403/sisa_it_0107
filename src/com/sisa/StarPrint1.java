f70ad5a2d7ce2785fc501b6148240b02ea2daa09
01_KDT <01_KDT@BOOK-UP5F0RS3G7>
2026-01-07 09:50:20 +0900

    라인정리_2025-01-07

--- src/com/sisa/StarPrint1.java
+++ src/com/sisa/StarPrint1.java
@@ -1,15 +1,14 @@
 
 package com.sisa;
 
-
 public class StarPrint1 {
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 
-        for (int i = 1; i <= 4; i++) {      // 줄 수
-            for (int j = 1; j <= i; j++) {  // 별 개수
-                System.out.print("*");
-            }
-            System.out.println();
-        }
-    }
+		for (int i = 1; i <= 4; i++) { // 줄 수
+			for (int j = 1; j <= i; j++) { // 별 개수
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
 }
--- src/com/sisa/StarPrint2.java
+++ src/com/sisa/StarPrint2.java
@@ -1,13 +1,16 @@
 package com.sisa;
 
 public class StarPrint2 {
-    public static void main(String[] args) {
-        for (int i = 4; i >= 1; i--) {      // 줄 수
-            for (int j = 1; j <= i; j++) {  // 별 개수
-                System.out.print("*");
-            }
-            System.out.println();
-        }
+	
+	public static void main(String[] args) {
+		
+		for (int i = 4; i >= 1; i--) { // 줄 수
+			
+			for (int j = 1; j <= i; j++) { // 별 개수
+				System.out.print("*");
+			}
+			System.out.println();
+		}
 
-    }
+	}
 }
